10e28008dfa9a37fbb74984c2f6a848fedc533ee
lesgreys <49399348+lesgreys@users.noreply.github.com>
2023-04-23 12:48:01 -0400

    add new mainnet peer, les

--- apps/hubble/src/allowedPeers.mainnet.ts
+++ apps/hubble/src/allowedPeers.mainnet.ts
@@ -23,4 +23,5 @@ export const MAINNET_ALLOWED_PEERS = [
   '12D3KooWHK4EXZ33nVFLSCLRuFNUK74v72eZEcRKoHTPbX9Nove7', // @neynar
   '12D3KooWDMXc4uVXr8zWGujaRuzhDnTBBHkTsJ58WimB8QU4cgos', // @nj
   '12D3KooWCZBZAirRXA2cecgud2zC7T5iN4hwr9nf7VAn9d8u9px2', // @rss3
+  '12D3KooWPaHFmDULq5ahsFM7TQ9tC3UmpiC5XUjtYe9xaoaw7iAs', // @les
 ];
